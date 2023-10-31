7b338bfe76ed944bfcc9c6d9073af0f4be77888a
mahdi <mahdimalvandi6@gmail.com>
2023-10-31 17:10:53 +0330

    change app url

--- first/first/urls.py
+++ first/first/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("app/", include("app1.urls", namespace="app1"))
+    path("", include("app1.urls", namespace="app1"))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
